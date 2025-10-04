67dcaea5f3d96bcafcf4882c24bc77b912992dd4
Oliwier Kwiatkowski <olikwiat@ThinkOlikwiat.www>
2025-10-04 15:18:17 +0200

    venv i szukacz danych & figures

---
+++ Codes/pierwyjkurve.py
@@ -0,0 +1,33 @@
+import os
+import lightkurve as lk
+import matplotlib.pyplot as plt
+
+#config
+target_name = "TOI-181"
+mission = "TESS"
+output_dir = "figures"
+output_file = "toi181_lightcurve.png"
+
+os.makedirs(output_dir, exist_ok=True)
+
+print(f"Searching for {target_name} from {mission}...")
+sr = lk.search_lightcurve(target_name, mission=mission)
+# print(f"Found {len(sr)} lightcurves.")
+print(sr)
+
+print("Downloading...")
+lc = sr[0].download()
+lc = lc.normalize()
+print("Done & normalized")
+
+plt.figure(figsize=(10,5))
+lc.plot()
+plt.title(f"{target_name} TESS Lightcurve")
+plt.xlabel("[BTJD]")
+plt.ylabel("Normalized Flux")
+plt.grid(True)
+
+save_path = os.path.join(output_dir, output_file)
+plt.savefig(save_path)
+plt.close()
+print(f"Lightcurve plot @ {save_path}")
